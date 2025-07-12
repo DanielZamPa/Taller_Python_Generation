74b5c0d8f572adbc9b0d8a8ec346cf412a7dea5b
Daniel Zamora Paez <[email>
2025-07-11 21:23:29 -0500

    Ejercicios del 1 al 5

---
+++ ejercicios.py
@@ -0,0 +1,33 @@
+#EJERCICIO 1, Suma de dos numeros
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+print("El resultado de la suma es: ", num1+num2)
+
+#EJERCICIO 2
+num1 = int(input("Ingrese un numero: "))
+if (num1%2==0):
+    print("El numero es par")
+elif (num1%2!=0):
+    print("El numero es impar")
+else:
+    print("Error")
+
+#EJERCICIO 3
+num1 = input("Ingrese el primer numero: ")
+num2 = input("Ingrese el segundo numero: ")
+num3 = input("Ingrese el trecer numero: ")
+
+if (num1>num2 and num1>num3):
+    print("El numero mayor es: ",num1)
+elif(num2>num1 and num2>num3):
+    print("El numero mayor es: ",num2)
+elif (num3>num1 and num3>num2):
+    print("El numero mayor es: ",num3)
+else:
+    print("Los 3 numeros son iguales.")
+
+#EJERCICIO 4
+num = int(input("Ingrese un numero: "))
+
+for c in range (1,11):
+    print(num, "*",c," = ",num*c)
